447473ff50f14d37707ced7d1cd678be96392b0d
Danah <osta.danah@gmail.com>
2022-10-27 23:34:36 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+/* 1) has a function expression called divide
+   2) divide divides 2000 by 100 */
+
+const divide = function (){
+    return 2000 / 100;
+}
+
+/* 1) has an arrow function called square
+   2) square arrow function takes one parameter and multiplies it times itself */
+
+const square = num => num * num;
+
+/* 1) has an arrow function called add
+   2) add arrow function takes two parameters and adds them together */
+
+const add = (num1, num2) => num1 + num2;   
